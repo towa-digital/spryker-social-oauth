4d0d458474b2d652449dbbbf8978d4a9601f86f4
Lukas Gabsi <lukas.gabsi@towa.at>
2022-10-20 07:57:47 +0200

    fix broken psr-4 path for constants

--- src/Towa/Shared/TowaSprykerOAuth/TowaSprykerOAuthConstants.php
+++ src/Towa/Shared/TowaSprykerOAuth/TowaSprykerOAuthConstants.php
@@ -5,7 +5,7 @@
  * For full license information, please view the LICENSE file that was distributed with this source code.
  */
 
-namespace Towa\Service\TowaSprykerOAuth;
+namespace Towa\Shared\TowaSprykerOAuth;
 
 interface TowaSprykerOAuthConstants
 {
